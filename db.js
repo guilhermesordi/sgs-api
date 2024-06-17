1331d1159e2397a53f18378e7fdded8a951b33c0
guilherme.sordi <guilherme.sordi@cresol.com.br>
2024-06-17 09:34:58 -0300

    feat: rollback

--- db.js
+++ db.js
@@ -1,12 +1,5 @@
 const pgp = require('pg-promise')();
-const db = pgp({
-  host: process.env.POSTGRES_HOST, 
-  database: process.env.POSTGRES_DATABASE, 
-  user: process.env.POSTGRES_USER, 
-  password: process.env.POSTGRES_PASSWORD,
-});
-
-// 'postgresql://postgres:Xt3"vS94qw-1".4t-&@db.vigcjlqbzziogtorkvxz.supabase.co:5432/postgres'
+const db = pgp('postgresql://postgres:Xt3"vS94qw-1".4t-&@db.vigcjlqbzziogtorkvxz.supabase.co:5432/postgres');
 
 module.exports = {
   db,
