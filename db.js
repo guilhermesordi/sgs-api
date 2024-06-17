d25608cce60009b8c95c7d6998ffed4fc862def0
guilherme.sordi <guilherme.sordi@cresol.com.br>
2024-06-17 08:58:13 -0300

    feat: changing db connection

--- db.js
+++ db.js
@@ -1,5 +1,13 @@
 const pgp = require('pg-promise')();
-const db = pgp('postgresql://postgres:Xt3"vS94qw-1".4t-&@db.vigcjlqbzziogtorkvxz.supabase.co:5432/postgres');
+const db = pgp({
+  host: 'db.vigcjlqbzziogtorkvxz.supabase.co', 
+  port: '5432', 
+  database: 'postgres', 
+  user: 'postgres', 
+  password: 'Xt3"vS94qw-1".4t-&'
+});
+
+// 'postgresql://postgres:Xt3"vS94qw-1".4t-&@db.vigcjlqbzziogtorkvxz.supabase.co:5432/postgres'
 
 module.exports = {
   db,
