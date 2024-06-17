3c7150351910fa8e64dedffb1da0c4b8a78081ac
guilherme.sordi <guilherme.sordi@cresol.com.br>
2024-06-17 09:08:58 -0300

    feat: use env

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 const pgp = require('pg-promise')();
 const db = pgp({
-  host: 'db.vigcjlqbzziogtorkvxz.supabase.co', 
-  port: '5432', 
-  database: 'postgres', 
-  user: 'postgres', 
-  password: 'Xt3"vS94qw-1".4t-&'
+  host: process.env.POSTGRES_HOST, 
+  database: process.env.POSTGRES_DATABASE, 
+  user: process.env.POSTGRES_USER, 
+  password: process.env.POSTGRES_PASSWORD,
 });
 
 // 'postgresql://postgres:Xt3"vS94qw-1".4t-&@db.vigcjlqbzziogtorkvxz.supabase.co:5432/postgres'
