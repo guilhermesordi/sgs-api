d385a9477b57d74e4b762a76e0e5af376b353d46
guilherme.sordi <guilherme.sordi@cresol.com.br>
2024-06-28 20:04:55 -0300

    feat: adding cors to the api service

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+const cors = require("cors");
 
 const app = express();
 const PORT = 4000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', routes);
 
